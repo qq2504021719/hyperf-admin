205491cd29abc161d533a483f3761c6e60a1e038
Eric.pan <Beats2019>
2020-07-09 16:54:59 +0800

    test

---
+++ publish/hyperf-admin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EricPan
+ * Date: 2020/7/9
+ * Time: 14:08
+ */
+
+return [
+
+    'route' => [
+
+        'prefix' => env('ADMIN_ROUTE_PREFIX', 'admin'),
+
+//    'namespace' => 'App\\Admin\\Controllers',
+
+//    'middleware' => ['web', 'admin'],
+    ],
+];
---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EricPan
+ * Date: 2020/7/9
+ * Time: 16:07
+ */
+
+namespace Pl\HyperfAdmin\Controllers;
+
+
+class HomeController
+{
+    public function index()
+    {
+        return ['home.index'];
+    }
+}
