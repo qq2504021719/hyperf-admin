bc2e6f39c72b7f549f3d35bcd23b83c9121c69a3
Eric.pan <Beats2019>
2020-08-07 17:45:10 +0800

    添加按钮未知错误

--- storage/view/content/add.blade.php
+++ storage/view/content/add.blade.php
@@ -1,1 +1,1 @@
-<a href="{{$add_url}}"><button type="button"  class="btn btn-{{$themeColor}}"><i class="fa fa-plus"  aria-hidden="true"></i> 导出</button></a>
+<a href="{{$add_url}}"><button type="button"  class="btn btn-{{$themeColor}}"><i class="fa fa-plus"  aria-hidden="true"></i> 添加</button></a>
